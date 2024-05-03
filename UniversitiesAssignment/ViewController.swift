936502d64551acd56d210f6ac0fd0d9a1d37beec
MoamenM <moamenabdelmaged@gmail.com>
2024-05-03 21:54:46 +0300

    Create Universities Assignment App

---
+++ UniversitiesAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversitiesAssignment
+//
+//  Created by Mo'men Abd Elmageed on 03/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
